6357fbc91a7e8b0e1c4e89cf7f95d58ebd74dc0a
Ghostwolf73 <solomzigumbo@gmail.com>
2023-03-30 13:08:00 +0200

    code is done but could use some optimization

--- login.py
+++ login.py
@@ -1,4 +1,32 @@
-username = input("Enter a username:")
-password = input("Enter a password:")
+while True:
+    
+    username = input("Enter a username:")
+    password = input("Enter a password:")
+    confirmPassword = input("Confirm your password:")
 
-print("Username " + username + "has been created")
+    if password == confirmPassword:
+        print("Username " + username + " has been created")
+        break
+    else:
+        print("Passwords do not match try again")
+
+while True:
+    loginUsername = input("Enter username to login:")
+    loginPassword = input("Enter password to login:")
+
+    if loginUsername == username and loginPassword == password:
+
+        print("Welcome " + username + " you have access to the server.")
+        break
+    
+    else:
+        print("Access denied, try again")
+
+
+        
+
+
+
+
+
+
